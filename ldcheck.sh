ce8af93ca8f53fa597d31852dcd65bbb38fa8352
David Wronek <david@mainlining.org>
2024-08-27 17:11:00 +0200

    ldcheck: Show symlinked libraries

--- ldcheck.sh
+++ ldcheck.sh
@@ -1,14 +1,22 @@
-#!/usr/bin/bash
+#!/usr/bin/sh
+# Recursively find if libraries are missing for Android binaries.
+# NOTE: This does not search for missing symbols, it will only show library files.
 FOUNDLIBS=""
 MISSINGLIBS=""
+SYMLINKLIBS=""
 
 getlibs () {
-	readelf -d $1 | grep '(NEEDED).*Shared library:' | awk -F'[\[\]]' '{print $2}'
+	readelf -d $1 | grep '(NEEDED).*Shared library:' | awk -F'[\\[\\]]' '{print $2}'
 }
 
 checkfile () {
-	ls $1 2> /dev/null &&
-	FOUNDLIBS="$FOUNDLIBS $1"
+	if [ -h $1 ]; then
+		echo $SYMLINKLIBS | grep -q $1 ||
+		SYMLINKLIBS="$SYMLINKLIBS $1"
+	else
+		ls $1 2> /dev/null &&
+		FOUNDLIBS="$FOUNDLIBS $1"
+	fi
 }
 
 checklibs () {
@@ -27,6 +35,7 @@ if [ ! -e $1 ]; then
 	exit 127
 fi
 
+echo "Found libraries:"
 checklibs $1
 
 if [ -n "$FOUNDLIBS" ]; then
@@ -39,6 +48,14 @@ if [ -n "$MISSINGLIBS" ]; then
 	echo -e "\n\033[0;93mMissing libraries:"
 	for i in $MISSINGLIBS; do
 		echo $i
+	done
+	echo -en "\033[0m"
+fi
+
+if [ -n "$SYMLINKLIBS" ]; then
+	echo -e "\n\033[0;35mSymlink libraries:"
+	for i in $SYMLINKLIBS; do
+		ls -l $i | awk '{for (i=9; i<NF; i++) printf $i " "; print $NF}'
 	done
 	echo -en "\033[0m"
 fi
